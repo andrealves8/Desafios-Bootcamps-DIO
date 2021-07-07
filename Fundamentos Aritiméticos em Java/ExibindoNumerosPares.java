66b847cf328665d28b93574d4b22620575679e43
André Alves <73435241+andrealves8@users.noreply.github.com>
2021-07-07 00:03:36 -0300

    Update ExibindoNumerosPares.java

--- Fundamentos Aritiméticos em Java/ExibindoNumerosPares.java
+++ Fundamentos Aritiméticos em Java/ExibindoNumerosPares.java
@@ -1,3 +1,24 @@
+/*
+Desafios
+Crie um programa que leia um número e mostre os números pares até esse número, inclusive ele mesmo.
+
+Entrada
+Você receberá 1 valor inteiro N, onde N > 0.
+
+Saída
+Exiba todos os números pares até o valor de entrada, sendo um em cada linha. 
+
+Exemplo de Entrada
+6
+
+Exemplo de Saída
+2
+4
+6
+
+*/
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
